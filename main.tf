352ef7b8d580a477c96c887a20c51bbba0dde011
Gustavo Bressan <gus7avo182@gmail.com>
2025-05-15 23:34:09 -0300

    update: incrementando e concluindo a aplicação

--- main.tf
+++ main.tf
@@ -13,17 +13,57 @@ terraform {
 // === PROVIDER ============================================
 
 // === EC2 INSTANCE ========================================
-resource "aws_instance" "web" {
+resource "aws_instance" "instance_ec2" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [aws_security_group.security_group_configs.id]
 
-  user_data = file("user_data.sh") # Opcional: instalar Apache, etc.
+  user_data = file("./files/user_data.sh") # Opcional: instalar Apache, etc.
 
   tags = {
-    Name = "TerraformWebServer"
+    Name = "Terraform-WebServer"
   }
 }
 // === EC2 INSTANCE ========================================
+
+// === SECURITY GROUP ======================================
+resource "aws_security_group" "security_group_configs" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+// === INGRESS =============================================
+resource "aws_vpc_security_group_ingress_rule" "ingress" {
+
+  for_each = {
+    for idx, rule in var.ingress_rules :
+    "${idx}" => rule
+  }
+
+  security_group_id = aws_security_group.security_group_configs.id
+
+  cidr_ipv4   = each.value.cidr_blocks
+  description = each.value.description
+  from_port   = each.value.from_port
+  ip_protocol = each.value.protocol
+  to_port     = each.value.to_port
+}
+// === INGRESS =============================================
+// === EGRESS ==============================================
+resource "aws_vpc_security_group_egress_rule" "egress" {
+  security_group_id = aws_security_group.security_group_configs.id
+
+  cidr_ipv4   = "10.0.0.0/8"
+  from_port   = 0
+  ip_protocol = "tcp"
+  to_port     = 0
+}
+// === EGRESS ==============================================
+// === SECURITY GROUP ======================================
--- variables.tf
+++ variables.tf
@@ -1,11 +1,30 @@
+// === EC2 INSTANCE ========================================
 variable "ami" {
   type = string
 }
-
 variable "instance_type" {
   type = string
 }
-
 variable "key_name" {
   type = string
 }
+// === EC2 INSTANCE ========================================
+
+// === SECURITY GROUP ======================================
+variable "vpc_id" {
+  type = string
+}
+// === INGRESS =============================================
+variable "ingress_rules" {
+  description = "Lista de regras de ingress para o SG"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = string
+    description = string
+  }))
+}
+// === INGRESS =============================================
+
+// === SECURITY GROUP ======================================
